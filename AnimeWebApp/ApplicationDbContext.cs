c77795f6ab7882c1a728724c28c05a376dbed28c
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-11-29 15:36:48 +0300

    fixing, add mainpage

---
+++ AnimeWebApp/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using AnimeWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimeWebApp
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ AnimeWebApp/Views/Home/MainPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AnimeWebApp.Views.Home
+{
+    public class MainPage : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
