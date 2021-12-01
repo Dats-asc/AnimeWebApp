32737729d9acf72486c00504e570ac17f564be82
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-12-01 13:43:41 +0300

    registration

---
+++ AnimeWebApp/Views/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AnimeWebApp.Views.Account
+{
+    public class Login : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ AnimeWebApp/Views/Account/Registration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AnimeWebApp.Views.Account
+{
+    public class Registration : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
